c7a5d318ad49c259303b8d1f42d786a275a4ab91
Theo McCaie <theo.mccaie@informaticslab.co.uk>
2016-12-20 14:42:52 +0000

    fix up run script

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-#docker run -d -p 9999:8888 -e KERAS_BACKEND=tensorflow --name fted -v `pwd`"/notebook":/notebook ermaker/keras-jupyter
+docker rm fted 2> /dev/null
 
+docker run -d -p 9999:8888 -e KERAS_BACKEND=tensorflow --name fted -v `pwd`"/notebook":/notebook ermaker/keras-jupyter
+echo "starting up..."
+sleep 8
 token=`docker logs 2>&1 fted | grep token | cut -d? -f2`
 host="localhost"
 if [[ ! -z  $DOCKER_HOST ]]; then
