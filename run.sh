dd597c2cd4573f3f5d070943a8a4ce3c9153bb6c
Theo McCaie <theo.mccaie@informaticslab.co.uk>
2016-12-20 13:49:44 +0000

    run script fix for password issue

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
-docker run -d -p 9999:8888 -e KERAS_BACKEND=tensorflow -v `pwd`"/notebook":/notebook ermaker/keras-jupyter
+#!/bin/bash
+#docker run -d -p 9999:8888 -e KERAS_BACKEND=tensorflow --name fted -v `pwd`"/notebook":/notebook ermaker/keras-jupyter
 
-echo "running at http://"`echo $DOCKER_HOST | cut -d: -f2 | cut -d/ -f 3`":9999"
-
+token=`docker logs 2>&1 fted | grep token | cut -d? -f2`
+host="localhost"
+if [[ ! -z  $DOCKER_HOST ]]; then
+  host=`echo $DOCKER_HOST | cut -d: -f2 | cut -d/ -f 3`
+fi
+echo "running at http://"$host":9999/?"$token
